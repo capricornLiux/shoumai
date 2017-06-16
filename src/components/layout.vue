d1f064b98eae92daccb9339d9f10c01e1514220a
刘鑫 <784532927@qq.com>
2017-06-16 19:06:30 +0800

    layout基本样式

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
-    <div class="app-header">header</div>
+    <div class="app-header">
+      <div class="app-head-inner">
+        <img src="../assets/logo.png" alt="">
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <div class="app-content">content</div>
-    <div class="app-footer">footer</div>
+    <div class="app-footer">
+      <p>&copy; 2017 Lance MIT</p>
+    </div>
   </div>
 </template>
 
 <script>
   export default{
-      data(){
-          return {
-
-          }
-      }
+    data(){
+      return {}
+    }
   }
 </script>
 
@@ -42,27 +55,104 @@
     font: inherit;
     vertical-align: baseline;
   }
+
   /* HTML5 display-role reset for older browsers */
   article, aside, details, figcaption, figure,
   footer, header, hgroup, menu, nav, section {
     display: block;
   }
+
   body {
     line-height: 1;
   }
+
   ol, ul {
     list-style: none;
   }
+
   blockquote, q {
     quotes: none;
   }
+
   blockquote:before, blockquote:after,
   q:before, q:after {
     content: '';
     content: none;
   }
+
   table {
     border-collapse: collapse;
     border-spacing: 0;
   }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  /*自己的样式*/
+  body {
+    background: #f0f2f5;
+    font-family: "Helvetica Neue", Helvetica, Arial, "Hiragino Sans GB", "Hiragino Sans GB W3", "Microsoft YaHei UI", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
+    font-size: 14px;
+    color: #444;
+  }
+
+  /*头部*/
+  .app-header{
+    background-color: #363636;
+    color: #b2b2b2;
+    height: 90px;
+    line-height: 90px;
+    width: 100%;
+  }
+
+  .app-head-inner{
+    width: 1200px;
+    height: 100%;
+    /*background-color: lightskyblue;*/
+    margin: 0 auto;
+  }
+
+  .app-head-inner img{
+    width: 50px;
+    /*height: 100%;*/
+    margin-top: 20px;
+  }
+
+  .head-nav{
+    float: right;
+    width: 200px;
+    height: 100%;
+  }
+
+  .head-nav .nav-list{
+    width: 100%;
+    height: 100%;
+  }
+
+  .head-nav .nav-list li{
+    float: left;
+    cursor: pointer;
+  }
+
+  .head-nav .nav-list li.nav-pile {
+    margin-right: 10px;
+    margin-left: 10px;
+  }
+
+
+  .app-footer{
+    width: 100%;
+    height: 80px;
+    text-align: center;
+    line-height: 80px;
+    background-color: #e3e4e8;
+    margin-top: 30px;
+  }
+
+
+
+
 </style>
+
