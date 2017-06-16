99398fcb8eb7cd7050874518b9ca3114a2117d89
刘鑫 <784532927@qq.com>
2017-06-16 18:27:42 +0800

    添加layout上下布局文件, 在main.js入口js文件中使用

---
+++ src/components/layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="app-header">header</div>
+    <div class="app-content">content</div>
+    <div class="app-footer">footer</div>
+  </div>
+</template>
+
+<script>
+  export default{
+      data(){
+          return {
+
+          }
+      }
+  }
+</script>
