99ac7107601c1d8da35636330be47f5f1eb06f2a
刘鑫 <784532927@qq.com>
2017-06-19 16:02:11 +0800

    使用计算型属性对表单进行校验

--- src/components/loginForm.vue
+++ src/components/loginForm.vue
@@ -33,7 +33,11 @@
       </div>
       <!--登录按钮结束-->
 
-      <!--<p>{{ errorText }}</p>-->
+      <!--点击登录按钮的时候进行的提示信息-->
+      <p>{{ loginBtnErrorText }}</p>
+      <!--点击登录按钮的时候进行的提示信息结束-->
+
+
     </div>
 
     <!--<p>login login</p>-->
@@ -44,8 +48,12 @@
   export default{
     data(){
       return {
+        // 双向绑定的数据, 用户输入的用户名和密码
         usernameModel: '',
-        passwordModel: ''
+        passwordModel: '',
+
+        // 点击登录按钮的的提示信息
+        loginBtnErrorText: ''
       }
     },
 
@@ -54,8 +62,28 @@
        * 点击登录的时候调用的方法
        */
       onLogin(){
-        console.log(this.usernameModel);
-        console.log(this.passwordModel);
+//        console.log(this.usernameModel);
+//        console.log(this.passwordModel);
+
+        // 如果用户名或者密码中有任何一个不符合规则的时候需要进行错误 提示, 不能沟进行提交
+        if(!this.userErrors.status || !this.passwordErrors.status){
+            // 进行提示
+          this.loginBtnErrorText = '用户名或者密码错误';
+        } else {
+            // 用户名和密码都符合规则了, 进行提交
+          this.loginBtnErrorText = ''; // 清空错误提示信息
+          this.$http.get('/api/login').then(function successCallBack(response) {
+
+
+            // 登录成功, 将登录后的数据传递给父组件
+            // 子组将向父组件传递数据, 需要使用emit方法机型数据传递
+            this.$emit('loginSuccess', response.data);
+
+          },function errorCallBack(error) {
+            console.log('登录失败');
+            console.log(error);
+          })
+        }
       },
     },
 
@@ -77,6 +105,15 @@
           errorText = '用户名中必须包含@'
         }
 
+        // 第一次弹出对话框的时候不显示提示信息(因为还没有进行输入)
+        if(!this.usernameFlag){
+            // 没有提示信息
+          errorText = '';
+
+          // 更改状态
+          this.usernameFlag = true;
+        }
+
         return {
           status,
           errorText
@@ -93,6 +130,12 @@
           // 匹配不通过
           status = false;
           errorText = '密码必须为6-8位';
+        }
+
+        // 第一次弹出对话框的时候不进行错误提示
+        if(!this.pwdFlag){
+            errorText = '';
+            this.pwdFlag = true;
         }
         return {
           status,
