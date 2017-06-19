1a17fdb0618f358aa660f8fea167641e48ff535a
刘鑫 <784532927@qq.com>
2017-06-19 15:25:53 +0800

    编写表单的基本校验规则(利用计算型属性)

--- src/components/loginForm.vue
+++ src/components/loginForm.vue
@@ -1,27 +1,38 @@
 <template>
   <div class="login-form">
     <div class="g-form">
+
+      <!--用户名-->
       <div class="g-form-line">
         <span class="g-form-label">用户名：</span>
         <div class="g-form-input">
           <input type="text"
                  v-model="usernameModel" placeholder="请输入用户名">
         </div>
-        <!--<span class="g-form-error">{{ userErrors.errorText }}</span>-->
+        <span class="g-form-error">{{ userErrors.errorText }}</span>
       </div>
+      <!--用户名结束-->
+
+
+      <!--密码-->
       <div class="g-form-line">
         <span class="g-form-label">密码：</span>
         <div class="g-form-input">
           <input type="password"
                  v-model="passwordModel" placeholder="请输入密码">
         </div>
-        <!--<span class="g-form-error">{{ passwordErrors.errorText }}</span>-->
+        <span class="g-form-error">{{ passwordErrors.errorText }}</span>
       </div>
+      <!--密码结束-->
+
+      <!--登录按钮-->
       <div class="g-form-line">
         <div class="g-form-btn">
           <a class="button" v-on:click="onLogin">登录</a>
         </div>
       </div>
+      <!--登录按钮结束-->
+
       <!--<p>{{ errorText }}</p>-->
     </div>
 
@@ -33,17 +44,64 @@
   export default{
     data(){
       return {
-        usernameModel: 'hello world',
-        passwordModel: 'pwd'
+        usernameModel: '',
+        passwordModel: ''
       }
     },
 
     methods: {
+      /**
+       * 点击登录的时候调用的方法
+       */
       onLogin(){
         console.log(this.usernameModel);
         console.log(this.passwordModel);
+      },
+    },
+
+    // 计算型属性, 属性的值动态地计算
+    computed: {
+      userErrors(){
+
+        // 定义计算型属性的返回值, 两个变量, 提示错误信息和错误状态
+        let status, errorText;
+
+        // 使用正则表达式对象的test()方法对输入的内容进行校验
+        if (/@/g.test(this.usernameModel)) {
+          // 匹配通过
+          status = true;
+          errorText = ''
+        } else {
+          // 匹配不通过
+          status = false;
+          errorText = '用户名中必须包含@'
+        }
+
+        return {
+          status,
+          errorText
+        }
+      },
+
+      passwordErrors(){
+        let status, errorText;
+        if (/^.{6,8}$/.test(this.passwordModel)) {
+          // 匹配通过
+          status = true;
+          errorText = ''
+        } else {
+          // 匹配不通过
+          status = false;
+          errorText = '密码必须为6-8位';
+        }
+        return {
+          status,
+          errorText
+        }
       }
-    }
+    },
+
+
   }
 </script>
 
@@ -77,6 +135,16 @@
 
   .g-form-btn {
     padding-left: 100px;
+    /*background-color: skyblue;*/
+  }
+
+  .g-form-btn a {
+
+    display: inline-block;
+    width: 80px;
+    height: 20px;
+    background-color: skyblue;
+
   }
 
   .g-form-error {
